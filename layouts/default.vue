0ad293d3f35a9cb7b22da145bd782cdaa2ed6e2d
mstfymrtc <mstfymrtci@gmail.com>
2020-04-05 03:56:38 +0300

    init 🍻

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app dark>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn icon @click="logOut">
+        <v-icon>logout</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        }
+      ],
+      miniVariant: false,
+      right: false,
+      rightDrawer: false,
+      title: 'Posts App'
+    }
+  },
+  methods: {
+    logOut() {
+      sessionStorage.removeItem('token')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
